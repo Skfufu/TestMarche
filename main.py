049042d2e2377b4cf2396190596d1b3775cb4caf
Skfufu <enzo.carapuce2011@gmail.com>
2025-11-19 22:18:47 +0100

    Initialize Discord bot with basic setup

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True  # selon ce que tu veux faire
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Connecté en tant que {bot.user}")
+
+bot.run("TON_TOKEN")
